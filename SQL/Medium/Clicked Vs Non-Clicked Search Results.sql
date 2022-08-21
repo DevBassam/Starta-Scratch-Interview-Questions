783b185abf7cc0a13aaaeebf4e920ac9463cc22b
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2022-08-21 10:16:16 +0200

    Create Clicked Vs Non-Clicked Search Results.sql

---
+++ SQL/Medium/Clicked Vs Non-Clicked Search Results.sql
@@ -0,0 +1,13 @@
+WITH results
+AS (
+    SELECT sum(CASE WHEN search_results_position >= 0
+                THEN 1 ELSE 0 END) AS total,
+           sum(CASE WHEN clicked = 1 AND search_results_position <= 3
+                THEN 1 ELSE 0 END) AS clicked,
+           sum(CASE WHEN clicked = 0 AND search_results_position <= 3
+                THEN 1 ELSE 0 END) AS notclicked
+    FROM fb_search_events
+    )
+SELECT (clicked * 1.0 / total) * 100 AS top_3_clicked,
+       (notclicked * 1.0 / total) * 100 AS top_3_notclicked
+FROM results
