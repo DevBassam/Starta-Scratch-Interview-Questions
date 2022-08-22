c7d548f771ad763b50826bf50d1619ea93ae529f
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2022-08-22 12:00:09 +0200

    Create Income By Title and Gender.sql

---
+++ SQL/Easy/Income By Title and Gender.sql
@@ -0,0 +1,13 @@
+SELECT se.employee_title,
+    se.sex,
+    AVG(se.salary + sb.total_bonus) AS total_compensation
+FROM sf_employee AS se
+INNER JOIN (
+    SELECT worker_ref_id,
+        SUM(bonus) AS total_bonus
+    FROM sf_bonus
+    GROUP BY worker_ref_id
+    ) AS sb
+    ON se.id = sb.worker_ref_id
+GROUP BY se.employee_title,
+    se.sex
