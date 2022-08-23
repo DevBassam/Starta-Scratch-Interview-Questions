a957e64b4fea6bf2c770ddfb38a7fc855049aac1
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2022-08-23 07:48:08 +0200

    Update Users By Average Session Time.sql

--- SQL/Medium/Users By Average Session Time.sql
+++ SQL/Medium/Users By Average Session Time.sql
@@ -1,26 +1,15 @@
+-- Problem ID: 10352
 SELECT user_id,
 	avg(session_time)
 FROM (
 	SELECT user_id,
 		extract(day FROM TIMESTAMP),
-		min(CASE 
-				WHEN action = 'page_exit'
-					THEN TIMESTAMP
-				END) - max(CASE 
-				WHEN action = 'page_load'
-					THEN TIMESTAMP
-				END) AS session_time
+		min(CASE WHEN action = 'page_exit' THEN TIMESTAMP END) - 
+	        max(CASE WHEN action = 'page_load' THEN TIMESTAMP END) AS session_time
 	FROM facebook_web_log
-	GROUP BY 1,
-		2
+	GROUP BY 1,2
 	HAVING (
-			min(CASE 
-					WHEN action = 'page_exit'
-						THEN TIMESTAMP
-					END) - max(CASE 
-					WHEN action = 'page_load'
-						THEN TIMESTAMP
-					END)
-			) IS NOT NULL
+			min(CASE WHEN action = 'page_exit' THEN TIMESTAMP END) -
+		        max(CASE WHEN action = 'page_load' THEN TIMESTAMP END)) IS NOT NULL
 	) AS session
 GROUP BY user_id
